75d4dd8f0e43b786c2afc6511c26846c0fecb8f1
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-02-21 21:19:09 +0300

    allowed methods

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request
+from flask import Flask, Response
 
 app = Flask(__name__)
 
@@ -8,7 +8,6 @@ def index() -> Response:
     return Response('Hello World!')
 
 
-@app.route('/<string:city>')
+@app.route('/<string:city>', methods=['GET', 'OPTIONS', 'HEAD'])
 def index_city(city: str) -> Response:
-    name = request.args.get('name', None)  # get value of query param
     return Response(f'Hello, {city}!')
