4c3329a2fe751b7f37af202080cec3e38c9c5ed3
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-02-21 20:58:12 +0300

    app init with comments

--- app/app.py
+++ app/app.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app = Flask(__name__)  # pass file name
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/')  # register route to run view
+def index():  # view
+    # return 'Hello World!', 200  # the same way
+    return Response('Hello World!', 200)  # data, response status code
