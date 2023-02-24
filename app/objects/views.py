b6b1f09b0ff14d1f1573887d8226fa4566a98ff1
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-02-24 11:38:48 +0300

    blueprints with generate url

--- app/objects/views.py
+++ app/objects/views.py
@@ -1,22 +1,24 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
 
 object_item = Blueprint(name="objects",
                         import_name=__name__,
                         url_prefix="/objects",
                         static_folder="../../static")
 
-OBJECTS = ("Apple", "Phone", "Book")
+OBJECTS = {1: "Apple", 2: "Phone", 3: "Book"}
 
 
-# localhost/object/
 @object_item.route("/")
 def object_list():
-    context = {
-        "objects": OBJECTS
-    }
-    return render_template("objects/list.html", **context)
+    return render_template("objects/list.html", objects=OBJECTS)
 
 
 @object_item.route("/<int:pk>")
 def get_object_item(pk: int):
-    return str(pk)
+    try:
+        object_inst = OBJECTS[pk]
+    except KeyError:
+        raise NotFound(f"Object ID {pk} not found")
+        # or redirect("/objects/")
+    return render_template("objects/details.html", object_inst=object_inst)
