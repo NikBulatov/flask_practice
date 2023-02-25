90a3129fc6b7eee72236ac95b59c81bc434540ba
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-02-25 11:13:23 +0300

    config to connect db

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from app.articles.views import articles
 from app.followers.views import followers
+from app.models.db import db
+from config import Config
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
     register_blueprints(app)
     return app
 
