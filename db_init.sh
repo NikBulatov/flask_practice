78c137c998561442be613c38e4ecb59e8dfc4213
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-07-30 20:01:34 +0300

    python3 deploy

--- db_init.sh
+++ db_init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+pip install -r requirements.txt
+
 python3 -m flask db init
 python3 -m flask db migrate
 python3 -m flask db upgrade
+
 python3 -m flask create-init-tags
 python3 -m flask create-init-user
 
